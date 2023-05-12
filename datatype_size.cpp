ea6b01c588df2e7b87ab754f3ecd892a16374904
MOURYA <120260373+MOURYAK0901@users.noreply.github.com>
2023-05-12 19:32:05 +0530

    Add files via upload

---
+++ datatype_size.cpp
@@ -0,0 +1,23 @@
+//printing size of datatypes             ~DATE-12/05/2023
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"size of integer "<<sizeof(a)<<endl;
+    
+    bool b;
+    cout<<"size of boolean "<<sizeof(b)<<endl;
+
+    float c;
+    cout<<"size of float "<<sizeof(c)<<endl;
+
+    char d;
+    cout<<"size of character "<<sizeof(d)<<endl;
+
+    double e;
+    cout<<"size of double "<<sizeof(e)<<endl;
+    
+    return 0;
+
+}
